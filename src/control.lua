6a104aaecd68fa762b8af824ccfdd96e68edeaa9
Rseding91 <Rseding91@yahoo.com>
2015-07-17 18:08:40 -0400

    Updated for 0.12.0

--- src/control.lua
+++ src/control.lua
@@ -1,12 +1,12 @@
 require "defines"
 
-game.onevent(defines.events.onchunkgenerated, function(event)
-	local distX = math.abs(event.area.lefttop.x)
-	local distY = math.abs(event.area.lefttop.y)
+game.on_event(defines.events.on_chunk_generated, function(event)
+	local distX = math.abs(event.area.left_top.x)
+	local distY = math.abs(event.area.left_top.y)
 	local mult = 1 + (math.floor(math.sqrt((distX * distX) + (distY * distY))) / 3500)
 	
 	if mult > 1 then
-		for _,ore in pairs(game.findentitiesfiltered({area = {{event.area.lefttop.x, event.area.lefttop.y}, {event.area.rightbottom.x, event.area.rightbottom.y}}, type="resource"})) do
+		for _,ore in pairs(event.surface.find_entities_filtered({area = {{event.area.left_top.x, event.area.left_top.y}, {event.area.right_bottom.x, event.area.right_bottom.y}}, type="resource"})) do
 			ore.amount = math.floor(ore.amount * mult)
 		end
 	end
